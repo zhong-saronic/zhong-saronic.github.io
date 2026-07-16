725f1ab795d24509b386b44a007f7b826df76991
Alfred Zhong <sir.alfred.zhong.1@gmail.com>
2026-07-15 22:38:16 -0500

    Hello world web app deployed to Github pages

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main style={{ fontFamily: "system-ui, sans-serif", textAlign: "center", marginTop: "20vh" }}>
+      <h1>Hello, World!</h1>
+      <p>React + TypeScript, hosted on GitHub Pages.</p>
+    </main>
+  );
+}
+
+export default App;
